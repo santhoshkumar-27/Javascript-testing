5ecbea178b28d5b1397e0f0d9c69af28e145b677
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-09 06:12:40 +0530

    feat: integrated the calculation part in the html

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-// import { Calculator } from "./calculator";
-
+const calculator = new Calculator();
+const operators = /\*|\+|\/|\-/;
+let result_number_ele;
 window.addEventListener('load', ($event) => {
-    document.getElementById('valueEnter').addEventListener('change', calculate)
+    document.getElementById('valueEnter').addEventListener('change', getInputFromUser)
+    result_number_ele = document.getElementById('result_number');
 })
-const calculator = new Calculator()
-function calculate(event) {
+function getInputFromUser(event) {
     const inputValue = event.target.value;
-    const operators = /\*|\+|\/|\-/;
+    if (!operators.test(inputValue)) {
+        result_number_ele.innerHTML = 'Invalid operator you entered';
+        return;
+    }
     const numbers = inputValue.split(operators);
     const num1 = +numbers[0];
     const num2 = +numbers[1];
-    const operator = inputValue.match(operators)[0];
-    console.log('inputValue', num1, num2, operator)
-    console.log(calculate(num1, num2));
+    const operator = inputValue.match(operators)[0].trim();
+    calculate(num1, num2, operator);
+    event.target.value = null;
+}
+
+function calculate(num1, num2, operator) {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = calculator.add(num1, num2);
+            break;
+        case '-':
+            result = calculator.subtract(num1, num2);
+            break;
+        case '*':
+            result = calculator.multiple(num1, num2);
+            break;
+        case '/':
+            result = calculator.multiple(num1, num2);
+            break;
+        default:
+            result = 'Invalid operator';
+    }
+    result_number_ele.innerHTML = result;
 }
