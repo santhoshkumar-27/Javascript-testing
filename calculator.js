06ef3e46f53b1b124ee59b0081ef1aa1d557748e
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-14 19:53:15 +0530

    checking the test case for the calculator

--- calculator.js
+++ calculator.js
@@ -1,19 +1,20 @@
-function Calculator() {
-    this.total = 0;
-}
-
-Calculator.prototype.add = function (num1, num2) {
-    return  num1 + num2;
-}
-Calculator.prototype.subtract = function (num1, num2) {
-    return  num1 - num2;
-}
-Calculator.prototype.divide = function (num1, num2) {
-    if (num2 === 0) {
-        throw new Error('value is not equal to zero')
+class Calculator {
+    constructor() {
+        this.total = 0;
     }
-    return  num1 / num2;
-}
-Calculator.prototype.multiple = function (num1, num2) {
-    return  num1 * num2;
+    add(num1, num2) {
+        return num1 + num2;
+    }
+    subtract(num1, num2) {
+        return num1 - num2;
+    }
+    divide(num1, num2) {
+        if (num2 === 0) {
+            throw new Error('value is not equal to zero');
+        }
+        return num1 / num2;
+    }
+    multiple(num1, num2) {
+        return num1 * num2;
+    }
 }
--- testing/spec/calculator.spec.js
+++ testing/spec/calculator.spec.js
@@ -1,18 +1,26 @@
 describe('calculator testing suites', () => { // this is test suite is group of test
     it('should sum the two number taking from paramaters', () => {
-
+        const calculator = new Calculator();
+        const result = calculator.add(5, 5)
+        expect(result).toBe(10);
     })
 
     it('should subtract the two number taking from paramaters', () => {
-
+        const calculator = new Calculator();
+        const result = calculator.subtract(5, 5)
+        expect(result).toBe(0);
     })
 
     it('should divide the two number taking from paramaters', () => {
-
+        const calculator = new Calculator();
+        const result = calculator.multiple(5, 5)
+        expect(result).toBe(25);
     })
 
     it('should multiply the two number taking from paramaters', () => {
-
+        const calculator = new Calculator();
+        const result = calculator.divide(5, 5)
+        expect(result).toBe(1);
     })
 });
 
