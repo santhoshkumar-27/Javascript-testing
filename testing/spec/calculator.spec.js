c8eff867f0886be2deda221516f68fc082f21c77
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-14 15:01:23 +0530

    added specs

---
+++ testing/spec/calculator.spec.js
@@ -0,0 +1,26 @@
+describe('calculator testing suites', () => { // this is test suite is group of test
+    it('should sum the two number taking from paramaters', () => {
+
+    })
+
+    it('should subtract the two number taking from paramaters', () => {
+
+    })
+
+    it('should divide the two number taking from paramaters', () => {
+
+    })
+
+    it('should multiply the two number taking from paramaters', () => {
+
+    })
+});
+
+// we can add more test suites like this
+/*
+describe('calculator testing suites', () => {
+    it('checking the sum isolated function taking two paramaters', () => {
+
+    })
+});
+*/
