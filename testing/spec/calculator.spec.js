859ed840e8c9b657001f8126438e0251a7d9dc01
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-19 06:06:49 +0530

    feat: nest suites

--- testing/spec/calculator.spec.js
+++ testing/spec/calculator.spec.js
@@ -1,62 +1,73 @@
 describe('calculator testing suites', () => { // this is test suite is group of test
-    it('should sum the two number taking from paramaters', () => {
-        const calculator = new Calculator();
-        const result = calculator.add(5, 5)
-        expect(result).toBe(10);
+
+    describe('calculator', () => {
+
+        describe('add()', () => {
+            it('should sum the two number taking from paramaters', () => {
+                const calculator = new Calculator();
+                const result = calculator.add(5, 5)
+                expect(result).toBe(10);
+            })
+        })
+
+        describe('subtract()', () => {
+            it('should subtract the two number taking from paramaters', () => {
+                const calculator = new Calculator();
+                const result = calculator.subtract(5, 5)
+                expect(result).toBe(0);
+            })
+        })
+        describe('multiple()', () => {
+            it('should multiply the two number taking from paramaters', () => {
+                const calculator = new Calculator();
+                const result = calculator.divide(5, 5)
+                expect(result).toBe(1);
+            })
+        })
+        describe('divide()', () => {
+            it('should divide the two number taking from paramaters', () => {
+                const calculator = new Calculator();
+                const result = calculator.multiple(5, 5)
+                expect(result).toBe(25);
+            })
+        })
+
+        it('should compare the objects', () => {
+            const calculator1 = new Calculator();
+            const calculator2 = new Calculator();
+            // expect(calculator2).toBe(calculator1);
+            expect(calculator2).toEqual(calculator1); // toEqual also check the expect and actual behaivor of
+            /*
+                compare key -> name and a type
+                compare value -> of same value and a type
+            */
+        });
+        it('should be the truthy', () => {
+            const calculator = new Calculator();
+            expect(calculator).toBeTruthy();
+        })
+        it('should be the falsy', () => {
+            const calculator = new Calculator();
+            expect(calculator.add(0, 0)).toBeFalsy();
+        })
+        xit('should be in disabled stated', () => {
+            const calculator = new Calculator();
+            const result = calculator.divide(5, 5)
+            expect(result).toBe(1);
+        })
+
+        xit('should compare non primitive type', () => {
+            let person1 = {
+                name: 'santhosh'
+            }
+            let person2 = {
+                name: 'santhosh'
+            }
+            // expect(person1).toBe(person2); // not same
+            expect(person1).toBe(person1); // now the tripple equal to check the memory address of the objects its same
+        })
     })
 
-    it('should subtract the two number taking from paramaters', () => {
-        const calculator = new Calculator();
-        const result = calculator.subtract(5, 5)
-        expect(result).toBe(0);
-    })
-
-    it('should divide the two number taking from paramaters', () => {
-        const calculator = new Calculator();
-        const result = calculator.multiple(5, 5)
-        expect(result).toBe(25);
-    })
-
-    it('should multiply the two number taking from paramaters', () => {
-        const calculator = new Calculator();
-        const result = calculator.divide(5, 5)
-        expect(result).toBe(1);
-    })
-
-    it('should compare the objects', () => {
-        const calculator1 = new Calculator();
-        const calculator2 = new Calculator();
-        // expect(calculator2).toBe(calculator1);
-        expect(calculator2).toEqual(calculator1); // toEqual also check the expect and actual behaivor of
-        /*
-            compare key -> name and a type
-            compare value -> of same value and a type
-        */
-    });
-    it('should be the truthy', () => {
-        const calculator = new Calculator();
-        expect(calculator).toBeTruthy();
-    })
-    it('should be the falsy', () => {
-        const calculator = new Calculator();
-        expect(calculator.add(0, 0)).toBeFalsy();
-    })
-    xit('should be in disabled stated', () => {
-        const calculator = new Calculator();
-        const result = calculator.divide(5, 5)
-        expect(result).toBe(1);
-    })
-
-    xit('should compare non primitive type', () => {
-        let person1 = {
-            name: 'santhosh'
-        }
-        let person2 = {
-            name: 'santhosh'
-        }
-        // expect(person1).toBe(person2); // not same
-        expect(person1).toBe(person1); // now the tripple equal to check the memory address of the objects its same
-    })
 });
 
 // we can add more test suites like this
